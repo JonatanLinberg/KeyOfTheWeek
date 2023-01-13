90100eee213a073582b1fee43b41691dd1294ac1
JonatanLinberg <98joli@gmail.com>
2023-01-13 15:09:12 +0100

    actual week calculation

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+
+// get week number 1-52 mon-sun
+function getweek(date) {
+	daylen = 86400000
+	if (date.getFullYear() % 4 == 0 && date.getMonth() >= 2) {
+		date.setDate(date.getDate() + 1) // leap year (works until 2100...)
+	}
+	var year_start = new Date(date.getFullYear(), 0, 1)
+	var day = Math.floor((date - year_start) / daylen)
+	return Math.ceil(day / 7)
+}
 const keys = ['F#', 'B', 'Bb', 'Db', 'C', 'Eb', 'F', 'G', 'D', 'Ab', 'E', 'A']
 const keydiv = document.getElementById("keydiv");
-keydiv.append(keys[0])
+
+i = getweek(new Date()) % keys.length
+keydiv.append(keys[i])
